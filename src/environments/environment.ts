dacf571079d7161103ef4732df4ce5b58c9317d2
Adriel Cardoso <adrmcardoso@outlook.com>
2021-02-15 18:34:48 -0300

    ajustes dos dados de variaveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase : {
-    apiKey: "AIzaSyAGdI6IhhEbhiFQZuBlKbgPCPrd7uEHKr4",
-    authDomain: "crudangular-a453b.firebaseapp.com",
-    databaseURL: "https://crudangular-a453b.firebaseio.com",
-    projectId: "crudangular-a453b",
-    storageBucket: "crudangular-a453b.appspot.com",
-    messagingSenderId: "673462276910",
-    appId: "1:673462276910:web:38d3261c2bd07a5ae2a0bf"
-  }
+
 };
 
 /*
